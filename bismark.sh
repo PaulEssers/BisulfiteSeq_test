7c91fd8594deeda82a848bec28db663be5a0121b
PaulEssers <paulessers@hotmail.com>
2015-10-22 16:05:25 +0200

    updated bismark script for paired end files

--- bismark.sh
+++ bismark.sh
@@ -14,20 +14,28 @@ mkdir ../bismark
 bsm=$(readlink -f ../bismark)
 cd ${rawt}
 
-for file in $(ls *.fastq); do echo "#!/bin/bash
+# only run once per pair of files
+for file in $(ls *.fastq); do echo ${file::(-11)}
+if [[ -e ${tmp}bismark_${file::(-11)}.sh ]]
+then echo "file already exists"
+else	
+echo ${tmp}bismark_${file::(-11)}.sh
+echo "#!/bin/bash
 cd ${rawt}
 module load Bowtie2
 module load Bismark
-bismark --bowtie2 -o ${bsm} ${tmp} ${rawt}${file}
-rm ${tmp}bismark_${file::(-5)}sh" > ${tmp}bismark_${file::(-5)}sh
+bismark --bowtie2 -o ${bsm} ${bsm} -1 ${rawt}${file::(-10)}1.fa.fastq -2 ${rawt}${file::(-10)}2.fa.fastq
+rm ${tmp}bismark_${file::(-11)}.sh" > ${tmp}bismark_${file::(-11)}.sh
 
 cd ${tmp}
-chmod 755 ${tmp}bismark_${file::(-5)}sh
-rm ../slurm_logs/bismark_${file::(-5)}*.out
-sbatch -p blade,himem,hugemem --cpus-per-task=18 -o ../slurm_logs/bismark_${file::(-5)}%j.out ${tmp}bismark_${file::(-5)}sh 2>&1 | tee ${tmp}bismark_${file::(-5)}id
-id=$(cat ${tmp}bismark_${file::(-5)}id | grep 'Submitted batch job')
+chmod 755 ${tmp}bismark_${file::(-11)}.sh
+rm ../slurm_logs/bismark_${file::(-11)}.*.out
+sbatch -p blade,himem,hugemem --cpus-per-task=18 -o ../slurm_logs/bismark_${file::(-11)}.%j.out ${tmp}bismark_${file::(-11)}.sh 2>&1 | tee ${tmp}bismark_${file::(-11)}.id
+id=$(cat ${tmp}bismark_${file::(-11)}.id | grep 'Submitted batch job')
 echo -n :${id:20} >> ${tmp}bismark.ids
-rm ${tmp}bismark_${file::(-5)}id
+rm ${tmp}bismark_${file::(-11)}.id
+
+fi
 done
 
 bismark_ids=$(cat ${tmp}bismark.ids)
